1fd3aef7e55dba9e42b6157ccb6ddfceccdad440
Andrew <andrewcohen14@gmail.com>
2021-03-04 23:55:51 -0500

    Auth Working, Login and Signup Forms Starter Styles

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import LogoutButton from './Forms/LogoutButton';
 import './NavBar.css'
 
 const NavBar = ({ setAuthenticated, authenticated, setDisplay }) => {
@@ -12,22 +12,26 @@ const NavBar = ({ setAuthenticated, authenticated, setDisplay }) => {
             Logo
           </NavLink>}
         </div>
-        {!authenticated ? (
         <div className="rightFraction">
-          <NavLink to="/login" exact={true} activeClassName="active">
+        {!authenticated && (
+        <div>
+         <NavLink to="/login" exact={true} activeClassName="active">
             Login
           </NavLink>
           <NavLink to="/sign-up" exact={true} activeClassName="active">
             Sign Up
           </NavLink>
           </div>
-          ) : (
-         <div className="rightFractionAuth">
-          <LogoutButton setAuthenticated={setAuthenticated} />
+        )}
         </div>
+        {/* <div>
+          {authenticated && (
+          <div>
+          <LogoutButton setAuthenticated={setAuthenticated} />
+          </div>
           )}
+          </div> */}
     </nav>
-  );
-}
+  )};
 
 export default NavBar;
--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,21 +1,39 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import {useDispatch } from "react-redux";
+import { Redirect, useHistory } from "react-router-dom";
 import { login } from "../../services/auth";
+import {setUser} from '../../store/session'
 
 const LoginForm = ({ authenticated, setAuthenticated }) => {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const onLogin = async (e) => {
     e.preventDefault();
     const user = await login(email, password);
     if (!user.errors) {
+      dispatchEvent(setUser(user))
       setAuthenticated(true);
+      history.push('/')
     } else {
       setErrors(user.errors);
     }
   };
+
+  const demoLogin = async (e) => {
+    e.preventDefault();
+    const demoUser = await login('demo@aa.io', 'password');
+    if (!demoUser.errors) {
+      dispatch(setUser(demoUser))
+      setAuthenticated(true);
+      history.push("/");
+    } else {
+      setErrors(demoUser.errors);
+    }
+  }
 
   const updateEmail = (e) => {
     setEmail(e.target.value);
@@ -29,36 +47,48 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
     return <Redirect to="/" />;
   }
 
+  const inputStyle = 'block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-gray-600 focus:ring-opacity-50" placeholder="Enter Your Username or Email'
+
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error) => (
-          <div>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type="submit">Login</button>
-      </div>
+    <div className="container flex justify-center items-center h-screen mx-auto">
+    <form onSubmit={onLogin} className="w-4/12 ">
+        <div className="relative w-full mt-10 space-y-8">
+            <div className="relative">
+                <ul>
+                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                </ul>
+                <label htmlFor="email" className="font-medium text-gray-900">Email</label>
+                <input
+                    name="email"
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={updateEmail}
+                    className={inputStyle}
+                />
+            </div>
+            <div className="relative">
+                <label htmlFor="password" className="font-medium text-gray-900">Password</label>
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={updatePassword}
+                    className={inputStyle} />
+            </div>
+            <div className="relative">
+                <button type="submit" className="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-black rounded-lg hover:bg-gray-700 ease">Log In</button>
+                <button
+                    type="submit"
+                    onClick={demoLogin}
+                    className="inline-block w-full px-5 py-4 mt-3 text-lg font-bold text-center text-gray-900 transition duration-200 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 ease">Demo User</button>
+            </div>
+        </div>
     </form>
+    </div>
   );
 };
+
 
 export default LoginForm;
