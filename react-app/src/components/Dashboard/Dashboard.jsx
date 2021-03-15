2726b85ebf09ef3da3d18f9c43197ad4dfad739e
Andrew <andrewcohen14@gmail.com>
2021-03-15 10:47:43 -0400

    rendering courses on the page

--- react-app/src/components/Dashboard/Dashboard.jsx
+++ react-app/src/components/Dashboard/Dashboard.jsx
@@ -2,12 +2,13 @@ import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {SideNav} from "../CommonElements";
 import {getUserCourses} from "../../store/course";
+import BookCover from "./BookCover.jpg";
+
 
 
 const Dashboard = ({setAuthenticated, authenticated}) => {
     const sessionUser = useSelector((state) => (state.session.user));
-    const courses = useSelector((state) => (state.course.userCourses))
-
+    const courses = useSelector((state) => Object.values(state.course.userCourses));
     console.log("COURSES: ",  courses)
     const dispatch = useDispatch();
 
@@ -17,17 +18,24 @@ const Dashboard = ({setAuthenticated, authenticated}) => {
 
 
     return (
-    <>
       <div className='grid grid-cols-12 w-full h-screen pt-20 bg-white-space overflow-hidden'>
-    <SideNav setAuthenticated={setAuthenticated} authenticated={authenticated}>
-    {courses && courses.map((course) => (
-        <ul className="col-start-7 col-end-8 mt-28">
-          <li key={course.id}>{course.name}</li>
-        </ul>
-      ))}
-      </SideNav>
-      </div>
-    </>
+      <SideNav setAuthenticated={setAuthenticated} authenticated={authenticated}/>
+      <ul className="mt-28 pl-40 h-64 grid grid-span-3 grid-flow-col gap-4">
+      {courses && courses.map(course => (
+          <li key={course.id}>
+    <div className="w-64 h-64 rounded overflow-hidden shadow-lg my-2">
+  <img className="w-full" src={BookCover} alt="Sunset in the mountains" />
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">{course.name}</div>
+    <p className="text-grey-darker text-base">
+      {course.description}
+    </p>
+  </div>
+  </div>
+  </li>
+  ))}
+  </ul>
+  </div>
     )
 };
 
--- react-app/src/components/Forms/NewCourseForm/NewCourseForm.jsx
+++ react-app/src/components/Forms/NewCourseForm/NewCourseForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import {useHistory} from "react-router-dom";
 import {useSelector, useDispatch} from "react-redux";
 import {createUserCourse} from "../../../store/course";
