934d46293ef92772962c0086c8195bd515ad7579
Andrew <andrewcohen14@gmail.com>
2021-04-11 22:24:03 -0400

    Delete Lesson Modal Clean Up

--- react-app/src/components/CourseBuilder/DeleteLesson/DeleteLessonForm.jsx
+++ react-app/src/components/CourseBuilder/DeleteLesson/DeleteLessonForm.jsx
@@ -22,7 +22,6 @@ const DeleteLessonForm = ({showModal, setShowModal, courseId, sectionId, id}) =>
       }}, [eventTrigger, dispatch, courseId]);
 
     const deleteThisLesson = async (e) => {
-        // console.log("LESSONID: ",typeof id)
         await dispatch(deleteOneUserCourseLesson({courseId: courseId,sectionId: sectionId, lessonId: id}))
         setEventTrigger(true)
       }
@@ -32,10 +31,10 @@ const DeleteLessonForm = ({showModal, setShowModal, courseId, sectionId, id}) =>
             <div className="w-full h-full text-center">
                 <div className="flex flex-col justify-between h-full">
                     <p className="mt-4 text-xl font-bold text-gray-800 dark:text-gray-200">
-                        Delete This Section
+                        Delete This Lesson
                     </p>
                     <p className="px-6 py-2 text-xs text-gray-600 dark:text-gray-400">
-                        Are you sure you want to delete this section?
+                        Are you sure you want to delete this lesson?
                     </p>
                     <div className="flex items-center justify-between w-full gap-4 mt-8">
                         <button
