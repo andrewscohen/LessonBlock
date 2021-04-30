d5306bc407f4855534ff9b34543b9ce94091cba3
Andrew <andrewcohen14@gmail.com>
2021-04-29 22:53:17 -0400

    added comments to navbar

--- react-app/src/components/Navigation/NavBar/NavBar.jsx
+++ react-app/src/components/Navigation/NavBar/NavBar.jsx
@@ -16,6 +16,7 @@ const NavBar = () => {
     <>
     {!sessionUser ? (
       <nav>
+        {/* START OF NOT LOGGED IN NAVBAR LAYOUT */}
         <ul className="fixed z-50 flex items-center justify-center w-full h-20 shadow-sm mobile:justify-between bg-brand-blue mobile:bg-transparent desktop:h-16 laptop:h-14 widescreen:h-20 laptop:pr-14 laptop:pl-12 desktop:px-28 widescreen:px-40 mobile:h-14 mobile:px-10">
           <li>
             <NavLink to="/" exact={true} activeClassName="active" className="text-3xl font-bold laptop:text-xl desktop:py-3 desktop:text-2xl mobile:text-lg font-monst text-white-space">LessonBlock</NavLink>
@@ -24,33 +25,36 @@ const NavBar = () => {
             <LoginFormModal />
           </li>
         </ul>
+        {/* END OF NOT LOGGED IN NAVBAR LAYOUT */}
       </nav>
     ) : (
-    <nav>
-      <ul className="fixed z-50 flex items-center justify-center w-full h-20 shadow-sm mobile:justify-between bg-brand-blue mobile:bg-white desktop:h-16 laptop:h-14 widescreen:h-20 mobile:h-14 mobile:px-10">
-        <li>
-          <NavLink
-            to="/dashboard"
-            exact={true}
-            activeClassName="active"
-            className="pr-4 font-bold text-grey-200 hover:text-indigo-500">
-              {`Hello, ${sessionUser.username}!`.toUpperCase()}
+      <nav>
+      {/* START OF LOGGED IN NAVBAR LAYOUT */}
+        <ul className="fixed z-50 flex items-center justify-center w-full h-20 shadow-sm mobile:justify-between bg-brand-blue mobile:bg-white desktop:h-16 laptop:h-14 widescreen:h-20 mobile:h-14 mobile:px-10">
+          <li>
+            <NavLink
+              to="/dashboard"
+              exact={true}
+              activeClassName="active"
+              className="pr-4 font-bold text-grey-200 hover:text-indigo-500">
+                {`Hello, ${sessionUser.username}!`.toUpperCase()}
+              </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/dashboard"
+              exact={true}
+              activeClassName="active"
+              className="block text-4xl font-bold hover:text-indigo-500 font-monst text-grey-200">
+                LessonBlock
             </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/dashboard"
-            exact={true}
-            activeClassName="active"
-            className="block text-4xl font-bold hover:text-indigo-500 font-monst text-grey-200">
-              LessonBlock
-          </NavLink>
-        </li>
-        <li>
-          <UserAvatarDropDown />
-        </li>
-      </ul>
-    </nav>
+          </li>
+          <li>
+            <UserAvatarDropDown />
+          </li>
+        </ul>
+        {/* END OF LOGGED IN NAVBAR LAYOUT */}
+      </nav>
     )}
 
 </>
